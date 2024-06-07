fd249e4c4a6ec045c30dc96ce5cd0e75252617e2
zxcdenis <mexzo0d@gmail.com>
2024-06-07 14:51:16 +0500

    docker docker docker host

--- news_portal/settings.py
+++ news_portal/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ukw_3@w8xlx6jeh5$4fp45s(%-pl@$w)$h84t2!6$new1wi3%g"
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ukw_3@w8xlx6jeh5$4fp45s(%-pl@$w)$h84t2!6$new1wi3%g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
